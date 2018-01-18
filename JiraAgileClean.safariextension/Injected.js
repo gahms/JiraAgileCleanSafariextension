8b8b57a8e891908043af91e4fd51b778623753a6
Nicolai Henriksen <nicolai@procrasty.com>
2018-01-18 18:01:56 +0100

    Ready for release

--- JiraAgileClean.safariextension/Injected.js
+++ JiraAgileClean.safariextension/Injected.js
@@ -5,16 +5,20 @@ function cleanJiraBoard() {
   hideElementsWithClass("iic-trigger");
   hideElementsWithClass("aui-item");
   hideElementsWithClass("ghx-linked aui-item");
+
+  // versions/epic sidebar - does not hide. Why?
   hideElementsWithClass("ghx-classification-menu-column");
+
+  // ghx-done should be marked with dimmed background color?
 }
 
 function hideElementsWithClass(className) {
-  console.log('hideElementsWithClass("' + className + '")');
+  // console.log('hideElementsWithClass("' + className + '")');
   var x = document.getElementsByClassName(className);
-  console.log("Found " + x.length);
+  // console.log("Found " + x.length);
   var i;
   for (i = 0; i < x.length; i++) {
-      x[i].classList.toggle("miracleTmpHidden");
+      x[i].classList.toggle("jiraAgileCleanTmpHidden");
   }
 }
 
--- JiraAgileClean.safariextension/main.js
+++ JiraAgileClean.safariextension/main.js
@@ -1,7 +1,7 @@
 safari.application.addEventListener("command", commandHandler, false);
 
 function commandHandler(event) {
-  console.log("commandHandler: " + event.command);
+  // console.log("commandHandler: " + event.command);
   if (event.command == "cleanJiraBoard") {
     safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("cleanJiraBoard");
   }
